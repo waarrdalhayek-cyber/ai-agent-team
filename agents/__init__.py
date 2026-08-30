18a469d1dfc8d72279239181cfc7eb0b4f6d8a2c
waarrdalhayek-cyber <waarrdalhayek@gmail.com>
2026-08-30 07:54:47 +0300

    Export TravelAgent and LearningAgent

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,14 @@ from .research_agent import ResearchAgent
 from .content_agent import ContentAgent
 from .execution_agent import ExecutionAgent
 from .video_agent import VideoAgent
+from .travel_agent import TravelAgent
+from .learning_agent import LearningAgent
 
-__all__ = ["ResearchAgent", "ContentAgent", "ExecutionAgent", "VideoAgent"]
+__all__ = [
+    "ResearchAgent",
+    "ContentAgent",
+    "ExecutionAgent",
+    "VideoAgent",
+    "TravelAgent",
+    "LearningAgent",
+]
