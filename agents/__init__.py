d259759b1295c0c87e1ecf8cacc32f85710e4baa
waarrdalhayek-cyber <waarrdalhayek@gmail.com>
2026-08-30 08:15:14 +0300

    Export division managers and EcommerceAgent

--- agents/__init__.py
+++ agents/__init__.py
@@ -4,12 +4,12 @@ from .execution_agent import ExecutionAgent
 from .video_agent import VideoAgent
 from .travel_agent import TravelAgent
 from .learning_agent import LearningAgent
+from .ecommerce_agent import EcommerceAgent
+from .business_manager import BusinessManager
+from .creative_learning_manager import CreativeLearningManager
 
 __all__ = [
-    "ResearchAgent",
-    "ContentAgent",
-    "ExecutionAgent",
-    "VideoAgent",
-    "TravelAgent",
-    "LearningAgent",
+    "ResearchAgent", "ContentAgent", "ExecutionAgent", "VideoAgent",
+    "TravelAgent", "LearningAgent", "EcommerceAgent",
+    "BusinessManager", "CreativeLearningManager",
 ]
